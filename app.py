0db2c9fee5e5ea5768bc4f8ccbd26ff82d8085a0
simonisfiction <simon.chesney@yahoo.com>
2026-04-01 04:12:22 -0700

    add basic to do list. add basic multiplication calculator

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = []
+mult_answer = 0
+
+
 
 @app.route('/templates')
-def index():
-  return render_template('user.html')
+def user():
+  print("entering the user view function")
+  return render_template('user.html', tasks = tasks)
 
-@app.route('/templates/<name>')
-def user(name):
-  return render_template('user.html', name = name)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+  task = request.form['task']
+  tasks.append(task)
+  return redirect(url_for('index'))
+
+
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
+  global mult_answer
+  num1 = int(request.form['input1'])
+  num2 = int(request.form['input2'])
+  print(f"{num1} {num2}")
+  mult_answer = num1 * num2
+  return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+  print("entering the index view function")
+  return render_template('user.html', tasks = tasks, mult_answer = mult_answer)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,7 +42,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-  return render_template('/templates'), 500
+  return render_template('500.html'), 500
 
 if __name__ == '__main__':
   app.run(debug=True)
