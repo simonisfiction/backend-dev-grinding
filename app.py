5a298666fb730a2dbcbd765714a9f6897c467c1f
simonisfiction <simon.chesney@yahoo.com>
2026-04-01 01:37:00 -0700

    add templates and basic routing with error page templates

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask 
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return '<h1>Hello world!' 
 
-# here is some syntax for dynamic urls
-# @app.route('/<name>')
-# def index(name):
-#   return '<h1>Hello, %s!</h1>' %name
+@app.route('/templates')
+def index():
+  return render_template('user.html')
+
+@app.route('/templates/<name>')
+def user(name):
+  return render_template('user.html', name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return render_template('/templates'), 500
 
 if __name__ == '__main__':
   app.run(debug=True)
